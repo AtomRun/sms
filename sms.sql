f04853081807c70134c17c5d9a7d1f1b56acf4f7
clee <lcup@foxmail.com>
2019-12-10 07:54:51 +0800

    update sql scripts

---
+++ sms.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql8-3306
+ Source Server Type    : MySQL
+ Source Server Version : 80015
+ Source Host           : localhost:3306
+ Source Schema         : sms
+
+ Target Server Type    : MySQL
+ Target Server Version : 80015
+ File Encoding         : 65001
+
+ Date: 10/12/2019 07:53:12
+*/
+
